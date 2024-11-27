6896064633cf18c01c005f5dc894e319040b2519
Valeriy Borzak <valeraborzak@gmail.com>
2024-11-27 23:07:02 +0100

    To-do list is already done, works

--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -1,29 +1,97 @@
-# When we start the programm we can see this To-Do list menu:
-#    1. View Tasks
-#    2. Add a Task
-#    3. Remove a Task
-#    4. Exit
-# Enter your choice: choose from the given 4 options
-# After each choice we have to see Todo list menu
+from termcolor import cprint, colored
 
-# 1. View Tasks:
-#   if list is empty We get a message: "No tasks in the list"
-#   otherwise: print a list of tasks
-#       // for index, task in enumerate(tasks, 1):
-#              -> index, task
 
-# 2. Add a task
-#   1. Get a message: "Enter a new task"
-#       if input not a string: print Error message /.lower().strip()
-#   2. Store added tasks in the database (define datastructure)
-
-# 3. Remove the task
-#   1. Get a message: "Enter the task number: " // only int(task number)
-#       if not an int from range of tasks:
-#           Error message
+MENU = "menu"
+TASKS = "tasks"
+PROMPTS = "prompts"
 
 todo = {
-    "menu": ["View Tasks", "Add a Task", "Remove a Task", "Exit"],
-    "tasks": [],
+    MENU: ["View Tasks", "Add a Task", "Remove a Task", "Exit"],
+    TASKS: [],
+    PROMPTS: [
+        "Enter your choice: ",
+        "Enter the task number: ",
+        "Enter a new task: ",
+    ],
 }
-print(todo["menu"])
+
+
+def display_menu():
+    print(colored("MENU", "cyan", attrs=["reverse"]))
+    for index, menu in enumerate(todo[MENU], 1):
+        print(f"{index}. {menu}")
+
+
+def get_user_choice(prompt, array) -> int:
+    while True:
+        try:
+            choice = int(input(colored(prompt, "green")))
+            if choice <= 0 or choice > len(array):
+                raise ValueError()
+            return choice
+        except ValueError:
+            cprint("Please, choose a number from the given options", "red")
+
+
+def get_task(prompt) -> str:
+    while True:
+        try:
+            task = input(colored(prompt, "yellow")).strip().capitalize()
+            if not task:
+                raise ValueError()
+            return task
+        except ValueError:
+            cprint("Enter a text", "magenta")
+
+
+def print_no_tasks():
+    cprint(
+        """
+No tasks in the list
+               """,
+        "yellow",
+    )
+
+
+def remove_task(todo, choice):
+    if not todo[TASKS]:
+        print_no_tasks()
+    else:
+        choice = get_user_choice(todo[PROMPTS][1], todo[TASKS])
+        todo[TASKS].pop(choice - 1)
+
+
+def view_tasks():
+    if todo[TASKS]:
+        print(colored("TASKS", "cyan", attrs=["reverse", "blink"]))
+        for index, task in enumerate(todo[TASKS], 1):
+            print(f"{index}. {task}")
+        print()
+
+    else:
+        print_no_tasks()
+
+
+def main():
+    while True:
+        display_menu()
+
+        choice = get_user_choice(todo[PROMPTS][0], todo[MENU])
+        if choice == 1:
+            view_tasks()
+
+        elif choice == 2:
+            task = get_task(todo[PROMPTS][2])
+            todo[TASKS].append(task)
+
+        elif choice == 3:
+            remove_task(todo, choice)
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
+    # print(bool(todo[TASKS]))
+    # print(bool(not todo[TASKS]))
