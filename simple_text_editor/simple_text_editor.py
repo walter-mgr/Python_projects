2c657376fa40ebb668dd76a829bc22db006b7c7a
Valeriy Borzak <valeraborzak@gmail.com>
2024-11-30 19:21:28 +0100

    STE: Define the task

---
+++ simple_text_editor/simple_text_editor.py
@@ -0,0 +1,29 @@
+# 1. Round / cycle
+
+# Enter the filename to open or create: somefile.txt
+
+# Enter your text (type SAVE on a new line to save and exist):
+#   Some text
+#   Some text
+#   Some text
+
+# SAVE
+
+# Recieve a masage: somefile.txt saved
+
+# 2. Round / cycle
+
+# Enter the filename to open or create: somefile.txt
+#   Some text   (old data)
+#   Some text   (old data)
+#   Some text   (old data)
+
+# Enter your text (type SAVE on a new line to save and exist):
+
+#   New data
+#   New data
+#   New data
+
+# SAVE
+
+# Recieve a masage: somefile.txt saved
