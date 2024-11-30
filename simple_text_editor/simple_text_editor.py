d0c4b5d4a72f6b7d992a5db332279bc4dd5c14aa
Valeriy Borzak <valeraborzak@gmail.com>
2024-11-30 22:15:16 +0100

    STE: figured out basics about working with files

--- simple_text_editor/simple_text_editor.py
+++ simple_text_editor/simple_text_editor.py
@@ -27,3 +27,35 @@
 # SAVE
 
 # Recieve a masage: somefile.txt saved
+
+from pathlib import Path
+
+file_path = Path("C:/Users/valer/Python_projects/simple_text_editor/test.txt")
+
+
+"C:/Users/valer/Python_projects/test.py"
+
+message = "Hi! I've just created a new file"
+
+
+def create_or_update_file(path, message):
+    try:
+        with open(path, "w") as file:
+            contents = file.write(f"{message}")
+            print(contents)
+    except FileExistsError:
+        print("The file doesn't exist.")
+
+
+def append_line_to_file(path):
+    try:
+        with open(path, "a") as file:
+            contents = file.write("\n'This is a message I write to the file'")
+            print(contents)
+
+    except FileNotFoundError:
+        print("The file does not exist.")
+
+
+# append_line_to_file(path)
+create_or_update_file(file_path, message)
