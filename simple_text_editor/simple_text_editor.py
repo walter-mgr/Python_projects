99b1b47a71ba6a0e314b0f55013a6cbd863a8f7a
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-02 16:54:44 +0100

    STE: Define the funciton to get valid filename from user

--- simple_text_editor/simple_text_editor.py
+++ simple_text_editor/simple_text_editor.py
@@ -1,6 +1,70 @@
-# 1. Round / cycle
+import os
+import re
+from termcolor import colored
+from enum import Enum
 
+
+class ConstantsColors:
+    COLOR_RED = "red"
+
+
+class ConstantsErrors:
+    INVALID_FILENAME_ERROR = "1"
+
+
+file_path = os.path.join(os.path.dirname(__file__), "test.txt")
+
+error_messages = {
+    "1": colored("Please enter a valid filename", ConstantsColors.COLOR_RED)
+}
+
+forbidden_chars_pattern = r"[<>:/\\|?*]"
+
+
+def is_valid_charachter(filename, forbidden_chars_pattern):
+    for char in filename:
+        if re.search(forbidden_chars_pattern, char):
+            return False
+    return True
+
+
+def is_valid_length(filename):
+    if len(filename) > 50:
+        return False
+    return True
+
+
+def is_valid_filename(filename, forbidden_chars_pattern):
+    if is_valid_charachter(filename, forbidden_chars_pattern) and is_valid_length(
+        filename
+    ):
+        return True
+
+
+def get_valid_filename():
+    while True:
+        try:
+            filename = input("Enter a filename: ").lower().strip()
+            if is_valid_filename(filename, forbidden_chars_pattern) and filename:
+                return filename
+            raise ValueError()
+        except ValueError:
+            print(error_messages[ConstantsErrors.INVALID_FILENAME_ERROR])
+
+
+def append_line_to_file(path, message):
+    try:
+        with open(path, "a") as file:
+            file.write(f"{message}")
+    except FileNotFoundError:
+        print("The file does not exist.")
+
+
+# 1. Round / cycle
 # Enter the filename to open or create: somefile.txt
+
+get_valid_filename()
+# get_file_name()
 
 # Enter your text (type SAVE on a new line to save and exist):
 #   Some text
@@ -28,34 +92,18 @@
 
 # Recieve a masage: somefile.txt saved
 
-from pathlib import Path
 
-file_path = Path("C:/Users/valer/Python_projects/simple_text_editor/test.txt")
-
-
-"C:/Users/valer/Python_projects/test.py"
-
-message = "Hi! I've just created a new file"
+message = "\nHi! I've just created a new file"
+# message = input()
 
 
 def create_or_update_file(path, message):
     try:
         with open(path, "w") as file:
-            contents = file.write(f"{message}")
-            print(contents)
-    except FileExistsError:
+            file.write(f"{message}")
+    except FileNotFoundError:
         print("The file doesn't exist.")
 
 
-def append_line_to_file(path):
-    try:
-        with open(path, "a") as file:
-            contents = file.write("\n'This is a message I write to the file'")
-            print(contents)
-
-    except FileNotFoundError:
-        print("The file does not exist.")
-
-
-# append_line_to_file(path)
-create_or_update_file(file_path, message)
+# create_or_update_file(file_path, message)
+# append_line_to_file(file_path, message)
