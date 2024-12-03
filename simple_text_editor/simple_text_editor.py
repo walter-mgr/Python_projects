ea1da4a42c3cca991249ac32358a56b1a22d6bed
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-03 20:26:21 +0100

    STE: task is done. Code is working. Next step: refactoring

--- simple_text_editor/simple_text_editor.py
+++ simple_text_editor/simple_text_editor.py
@@ -1,18 +1,18 @@
 import os
 import re
-from termcolor import colored
+from termcolor import colored, cprint
 from enum import Enum
 
 
 class ConstantsColors:
     COLOR_RED = "red"
+    COLOR_GREEN = "green"
+    COLOR_CYAN = "cyan"
 
 
 class ConstantsErrors:
     INVALID_FILENAME_ERROR = "1"
 
-
-file_path = os.path.join(os.path.dirname(__file__), "test.txt")
 
 error_messages = {
     "1": colored("Please enter a valid filename", ConstantsColors.COLOR_RED)
@@ -52,19 +52,51 @@ def get_valid_filename():
             print(error_messages[ConstantsErrors.INVALID_FILENAME_ERROR])
 
 
-def append_line_to_file(path, message):
+"""
+def open_or_update_file(path, message):
     try:
         with open(path, "a") as file:
-            file.write(f"{message}")
+            file.write(f"{message}\n")
     except FileNotFoundError:
         print("The file does not exist.")
+"""
+
+
+def open_or_update_file(file_path):
+    cprint(
+        "\nEnter your text (type SAVE on a new line to save and exit):",
+        ConstantsColors.COLOR_GREEN,
+    )
+    with open(file_path, "w") as file:
+        while True:
+            try:
+                line = input()
+                if line.upper() == "SAVE":
+                    break
+                file.write(line + "\n")
+
+            except FileNotFoundError:
+                print("The file does not exist.")
 
 
 # 1. Round / cycle
 # Enter the filename to open or create: somefile.txt
+def main():
 
-get_valid_filename()
-# get_file_name()
+    filename = get_valid_filename()
+    file_path = os.path.join(os.path.dirname(__file__), filename)
+
+    open_or_update_file(file_path)
+    # enter_text(file_path)
+    cprint(f"{filename} saved", ConstantsColors.COLOR_CYAN)
+
+
+main()
+
+# file_path = os.path.join(os.path.dirname(__file__), "test.txt")
+# text = enter_text()
+# open_or_update_file(file_path, text)
+# print(f"\n{text}")
 
 # Enter your text (type SAVE on a new line to save and exist):
 #   Some text
@@ -91,19 +123,3 @@ get_valid_filename()
 # SAVE
 
 # Recieve a masage: somefile.txt saved
-
-
-message = "\nHi! I've just created a new file"
-# message = input()
-
-
-def create_or_update_file(path, message):
-    try:
-        with open(path, "w") as file:
-            file.write(f"{message}")
-    except FileNotFoundError:
-        print("The file doesn't exist.")
-
-
-# create_or_update_file(file_path, message)
-# append_line_to_file(file_path, message)
