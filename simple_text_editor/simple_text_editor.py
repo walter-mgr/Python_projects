7084e11ebff0efb2ac6b2ed1c540ebb1b91cedc0
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-04 14:31:46 +0100

    STE: improve 'read_from_existing_file' function

--- simple_text_editor/simple_text_editor.py
+++ simple_text_editor/simple_text_editor.py
@@ -3,24 +3,27 @@ import re
 from termcolor import colored, cprint
 
 
-class ConstantsColors:
+class ConstColors:
     COLOR_RED = "red"
     COLOR_GREEN = "green"
     COLOR_CYAN = "cyan"
     COLOR_YELLOW = "yellow"
 
 
-class ConstantsErrors:
+class ConstErrors:
     INVALID_FILENAME_ERROR = "1"
 
 
-class ConstantsCommands:
+class ConstCommands:
     SAVE = "SAVE"
 
 
-error_messages = {
-    "1": colored("Please enter a valid filename", ConstantsColors.COLOR_RED)
-}
+class ConstFileEditMode:
+    FILE_WRITE = "w"
+    FILE_READ = "r"
+
+
+error_messages = {"1": colored("Please enter a valid filename", ConstColors.COLOR_RED)}
 
 forbidden_chars_pattern = r"[<>:/\\|?*]"
 
@@ -33,78 +36,73 @@ def is_valid_character(filename, forbidden_chars_pattern):
 
 
 def is_valid_length(filename):
-    if len(filename) > 50:
-        return False
-    return True
+    return len(filename) <= 50
 
 
 def is_valid_filename(filename, forbidden_chars_pattern):
-    if is_valid_character(filename, forbidden_chars_pattern) and is_valid_length(
+    return is_valid_character(filename, forbidden_chars_pattern) and is_valid_length(
         filename
-    ):
-        return True
-    return False
+    )
 
 
 def get_valid_filename():
     while True:
-        try:
-            filename = (
-                input(colored("\nEnter a filename: ", ConstantsColors.COLOR_YELLOW))
-                .lower()
-                .strip()
-            )
-            if is_valid_filename(filename, forbidden_chars_pattern) and filename:
-                return filename
-            raise ValueError()
-        except ValueError:
-            print(error_messages[ConstantsErrors.INVALID_FILENAME_ERROR])
+        filename = (
+            input(colored("\nEnter a filename: ", ConstColors.COLOR_YELLOW))
+            .lower()
+            .strip()
+        )
+        if is_valid_filename(filename, forbidden_chars_pattern) and filename:
+            return filename
+        print(error_messages[ConstErrors.INVALID_FILENAME_ERROR])
 
 
-def read_from_existing_file(file_path):
-
+def read_from_existing_file(file_path, filename):
+    print_cyan = colored(filename, ConstColors.COLOR_CYAN)
     try:
-        with open(file_path, "r") as file:
+
+        with open(file_path, ConstFileEditMode.FILE_READ) as file:
             contents = file.read()
+            if not contents:
+                cprint("\nNo content in this file", ConstColors.COLOR_YELLOW)
             print(f"\n{contents}")
+
     except FileNotFoundError:
-        print("File does not exist.")
+        cprint(
+            f"\nFile '{print_cyan}' was successfully created.\n",
+            ConstColors.COLOR_GREEN,
+        )
 
 
-def open_or_update_file(file_path):
+def open_and_update_file(file_path, filename):
     if file_path:
-        read_from_existing_file(file_path)
-
+        read_from_existing_file(file_path, filename)
     cprint(
-        "\nEnter your text (type SAVE on a new line to save and exit):",
-        ConstantsColors.COLOR_GREEN,
+        "Enter your text (type SAVE on a new line to save and exit):",
+        ConstColors.COLOR_GREEN,
     )
 
-    with open(file_path, "w") as file:
+    with open(file_path, ConstFileEditMode.FILE_WRITE) as file:
         while True:
-            try:
-                line = input()
-                if line.upper() == ConstantsCommands.SAVE:
-                    break
-                file.write(line + "\n")
 
-            except FileNotFoundError:
-                print("File does not exist.")
+            line = input()
+            if line.upper() == ConstCommands.SAVE:
+                break
+            file.write(line + "\n")
+        cprint(f"{filename} saved", ConstColors.COLOR_CYAN)
+
+    # print("File does not exist.")
 
 
 def main():
-
     filename = get_valid_filename()
-
     file_path = os.path.join(os.path.dirname(__file__), filename)
-
-    open_or_update_file(file_path)
-
-    cprint(f"{filename} saved", ConstantsColors.COLOR_CYAN)
+    open_and_update_file(file_path, filename)
 
 
 if __name__ == "__main__":
     main()
+    # print(get_valid_filename())
 
 
 """
