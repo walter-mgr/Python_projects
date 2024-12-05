7c58821037c515aef64f3ddb8df4886f87931ec0
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-05 20:32:10 +0100

    Start a new project: Pig Dice Game

---
+++ pig_dice_game/pig_dice_game.py
@@ -0,0 +1,32 @@
+"""
+# TASK DESCRIPTION
+
+# While Loop
+
+    # Player 1's turn
+    #   You rolled a 6
+    #   Roll again? (y/n): y
+    #   You rolled a 3
+    #   Roll again? (y/n): y
+    #   You rolled a 5
+    #   Roll again? (y/n): y
+
+    # Points are collecting 6 + 3 + 5
+    #   If roll a 1 all points are gone
+    #       Turn ends
+
+    # Messages:
+    #   You scored 14 points this turn.
+    #   Current scores: Player 1: 14, Player 2: 0
+
+    # Player 2's turn
+    #   You rolled a 6
+    #   Roll again? (y/n): y
+    #   You rolled a 1
+
+    # Messages:
+    #   You scored 0 points this turn.
+    #   Current scores: Player 1: 14, Player 2: 0
+
+    # The game repeats until one of the player reaches 100 points
+"""
