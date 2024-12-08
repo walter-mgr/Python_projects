5dc9312f9b73e1e0208aa6e52e4c51e32c9807ba
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-08 16:02:31 +0100

    PDG: hard reset version. If dice == 1, all scores are gone. Player starts again

--- pig_dice_game/pig_dice_game.py
+++ pig_dice_game/pig_dice_game.py
@@ -5,6 +5,7 @@ PLAYER_ONE = "1"
 PLAYER_TWO = "2"
 NO = "n"
 EXIT = "q"
+GAME_LIMIT = 50
 
 
 class ConstColors:
@@ -13,65 +14,43 @@ class ConstColors:
     COLOR_RED = "red"
 
 
-class ConstPlayersScore:
-    PLAYER_ONE_SCORE = colored(0, ConstColors.COLOR_CYAN)
-    PLAYER_TWO_SCORE = colored(0, ConstColors.COLOR_GREEN)
-
-
-"""
-def switch_player():
-    current_player = None
-    current_player = PLAYER_TWO if current_player == PLAYER_ONE else PLAYER_ONE
-    return current_player
-"""
-
-
-def add_scores(player, score_one, score_two, dice):
-    if player == PLAYER_ONE:
-        score_one += dice
-        # current_score
-        return score_one
-    else:
-        score_two += dice
-        return score_two
-
-
 def main():
-    score_one = 0
-    score_two = 0
+    total_score_one = 0
+    total_score_two = 0
 
     current_player = PLAYER_ONE
-    current_score = score_one
+    current_score = total_score_one
 
     print(f"Player {current_player}'s turn")
 
     while True:
+
         dice = randint(1, 6)
 
-        if current_player == PLAYER_ONE:
-            score_one += dice
-            current_score = score_one
-        else:
-            score_two += dice
-            current_score = score_two
-
         if dice == 1:
-            if current_player == PLAYER_ONE:
-                # score_one = 0
-                # current_score = score_one
-                current_score = 0
-            else:
-                # score_two = 0
-                # current_score = score_two
-                current_score = 0
 
-            cprint(f"\nYou rolled a {dice}", ConstColors.COLOR_RED)
+            if current_player == PLAYER_ONE:
+                current_score = 0
+                total_score_one = current_score
+            else:
+                current_score = 0
+                total_score_two = current_score
+
+            cprint(f"You rolled a {dice}", ConstColors.COLOR_RED)
             print(f"\nYou scored {current_score} points this turn.")
-            print(f"Current scores: Player 1: {score_one}, Player 2: {score_two}\n")
+            print(
+                f"Current scores: Player 1: {total_score_one}, Player 2: {total_score_two}\n"
+            )
             current_player = PLAYER_TWO if current_player == PLAYER_ONE else PLAYER_ONE
             print(f"Player {current_player}'s turn")
             continue
 
+        if current_player == PLAYER_ONE:
+            total_score_one += dice
+            current_score = total_score_one
+        else:
+            total_score_two += dice
+            current_score = total_score_two
         print(f"You rolled a {dice}")
 
         player_choice = input("Roll again? (y/n): ").lower().strip()
@@ -81,10 +60,15 @@ def main():
         if player_choice == NO:
             print(f"\nYou scored {current_score} points this turn.")
             if current_player == PLAYER_ONE:
-                score_one = current_score
+                total_score_one = current_score
             else:
-                score_two = current_score
-            print(f"Current scores: Player 1: {score_one}, Player 2: {score_two}\n")
+                total_score_two = current_score
+            print(
+                f"Current scores: Player 1: {total_score_one}, Player 2: {total_score_two}\n"
+            )
+            if total_score_one >= 50 or total_score_two >= 50:
+                cprint(f"Player {current_player} is winner!")
+                break
             current_player = PLAYER_TWO if current_player == PLAYER_ONE else PLAYER_ONE
             print(f"Player {current_player}'s turn")
 
@@ -132,4 +116,26 @@ main()
     #   Current scores: Player 1: 14, Player 2: 0
 
     # The game repeats until one of the player reaches 100 points
+"""
+
+"""
+1. This version resets total player's score 
+
+if dice == 1:
+    if current_player == PLAYER_ONE:
+        total_score_one = 0
+        current_score = total_score_one
+
+    else:
+        total_score_two = 0
+        current_score = total_score_two
+
+2. This version resets only current player's score
+
+if dice == 1:
+    if current_player == PLAYER_ONE:
+        current_score = 0
+    else:
+        current_score = 0
+
 """
