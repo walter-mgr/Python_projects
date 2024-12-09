3e8ab082e7a3cc07e9d946fccc0fbf8c9910bc74
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-09 20:02:58 +0100

    Start a new project Cows and Bulls

---
+++ cows_and_bulls/cows_and_bulls.py
@@ -0,0 +1,34 @@
+"""
+#   When run a program computer generates a 4-digit number with unique digits
+#   User need to guess a number with the computer hints
+
+    # Get valid input:
+        # Not an integer, not a string, not a double or tripple digits
+
+    #  Guess: 1123
+    #  Invalid guess. Please enter a 4-digit number with unique digits.
+    
+    #  Guess: 1234
+        2 cows, 1 bulls / one of the digits is in the correct position
+                          two digits that a right, but in the wrong position
+
+    #  Guess: 4321
+        3 cows, 0 bulls / 0 of the digits on the correct position
+
+    #  Guess: 4231
+        2 cows, 1 bulls
+
+    #  Guess: 1243
+        0 cows, 3 bulls
+
+    #  Guess: 4321
+        2 cows, 1 bulls
+
+    #  Guess: 4321
+        0 cows, 3 bulls
+
+    #  Guess: 9321
+        0 cows, 4 bulls
+    
+                        
+"""
