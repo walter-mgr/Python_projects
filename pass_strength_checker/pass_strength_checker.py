12a08311ac4e21c0b5aeeef229baf1295540b86c
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-12 01:22:32 +0100

    PSC: updated, validate user input, add colored output associated with the password strength level.

--- pass_strength_checker/pass_strength_checker.py
+++ pass_strength_checker/pass_strength_checker.py
@@ -13,8 +13,8 @@ it accordingly.
 Password Strength Criteria:
 - Very Weak: 4 digits
 - Weak: 8 digits
-- Medium: 8 digits with at least one uppercase letter
-- Strong: More than 6 characters with at least one uppercase and one lowercase letter
+- Medium: 5 digits with at least one uppercase letter
+- Strong: More than 5 digits with at least one uppercase and one lowercase letter
 - Very Strong: More than 5 characters with at least one uppercase letter, one lowercase letter, and at least one special symbol
 
 User Input:
@@ -24,14 +24,32 @@ User Input:
 """
 
 import re
+from termcolor import cprint, colored
+
+
+class ConstColors:
+
+    COLOR_CYAN = "cyan"
+    COLOR_YELLOW = "yellow"
+    COLOR_GREEN = "green"
+    COLOR_RED = "red"
+    COLOR_LIGHT_MAGENTA = "light_magenta"
+
+
+class ConstStrongCriteria:
+    VERY_STRONG = "Very Strong"
+    STRONG = "Strong"
+    MEDIUM = "Medium"
+    WEAK = "Weak"
+    VERY_WEAK = "Very Weak"
 
 
 strength_criteria = {
-    "Very Strong": r"^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*(),.?:{}|<>]).{5,}$",
-    "Strong": r"^(?=.*[A-Z])(?=.*[a-z]).{6,}$",
-    "Medium": r"^(?=.*[A-Z]).{7,}$",
-    "Weak": r"^\d{8}$",
-    "Very Weak": r"^\d{4}$",
+    ConstStrongCriteria.VERY_STRONG: r"^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*(),.?:{}|<>]).{5,}$",
+    ConstStrongCriteria.STRONG: r"^(?=.*[A-Z])(?=.*[a-z]).{5,}$",
+    ConstStrongCriteria.MEDIUM: r"^(?=.*[A-Z]).{5,}$",
+    ConstStrongCriteria.WEAK: r"^\d{8}$",
+    ConstStrongCriteria.VERY_WEAK: r"^\d{4}$",
 }
 
 
@@ -42,30 +60,44 @@ def check_password_strength(password: str, strength_criteria: dict) -> str:
     return "Very Weak"
 
 
+def get_valid_user_input():
+    while True:
+        user_input = input(
+            colored("\nEnter a password: ", ConstColors.COLOR_CYAN)
+        ).strip()
+        if len(user_input) >= 4:
+            return user_input
+        cprint(
+            "Password is too short. Please enter at least 4 characters",
+            ConstColors.COLOR_RED,
+        )
+
+
+def print_color(strong_pass):
+
+    color = {
+        ConstStrongCriteria.VERY_STRONG: ConstColors.COLOR_GREEN,
+        ConstStrongCriteria.STRONG: ConstColors.COLOR_GREEN,
+        ConstStrongCriteria.MEDIUM: ConstColors.COLOR_YELLOW,
+        ConstStrongCriteria.WEAK: ConstColors.COLOR_LIGHT_MAGENTA,
+        ConstStrongCriteria.VERY_WEAK: ConstColors.COLOR_RED,
+    }[strong_pass]
+    return color
+
+
 def main():
     while True:
-        password = input("Enter a password: ")
+        password = get_valid_user_input()
+
         strong_pass = check_password_strength(password, strength_criteria)
-        if strong_pass == "Very Strong":
-            print(f"Your password is {strong_pass}")
+
+        color = print_color(strong_pass)
+
+        cprint(f"Your password is {strong_pass}", color)
+
+        if strong_pass == ConstStrongCriteria.VERY_STRONG:
             break
-        print(f"Your password is {strong_pass}")
 
 
 if __name__ == "__main__":
     main()
-
-
-# Create a regex pattern for checking the password strength
-
-# Take user input: Enter a password: 1234
-#   # If input is 4 digits
-#       # Very week
-#   # If input is 8 digits
-#       # Week
-#   # If input is 8 digits and Uppercase Letter
-#       # Medium
-#   # If input is > 6 digits and Uppercase and Lowercase letter
-#       # Strong
-#   # If input is > 5 digits and Uppercase and Lowercase and >= 1 special symbol
-#       # Vrey strong
