c2399baf7a275bfd5d06bb0e926aa2e2d90e1207
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-12 14:17:40 +0100

    Start a new project: Password Generator

---
+++ pass_generator/pass_generator.py
@@ -0,0 +1,13 @@
+"""Password Generator Programm"""
+
+# Define a data structure for this project
+
+# Prompt user to enter a password length: 8
+
+# Prompt user to include uppercase letters? (y/n): y
+
+# Prompt user to include numbers? (y/n): y
+
+# Prompt user to include special characters? (y/n): y
+
+# Cenerate a password
