0ca96aa274deb2583e5c30e39aec22a0a2587280
Valeriy Borzak <valeraborzak@gmail.com>
2024-12-16 01:02:28 +0100

    PG: simplified 'get_sequence' function, add type annotations, docstring for functions

--- pass_generator/pass_generator.py
+++ pass_generator/pass_generator.py
@@ -1,30 +1,31 @@
 """Password Generator Programm"""
 
-import re
 import string
 import random
+from termcolor import cprint
 
 
 class ConstCommands:
-
     YES = "y"
+
+
+class ConstColors:
+    COLOR_RED = "red"
 
 
 class ConstPassConfig:
     MIN_LENGTH = 4
     LENGTH = "length"
     CONFIG_OPTIONS = "options"
+
     STRING_LOWER = "lower"
     STRING_UPPER = "upper"
     STRING_NUMBERS = "numbers"
     STRING_SYMBOLS = "symbols"
 
-
-class ConstPatternStrong:
-    WEAK = "weak"
-    MEDIUM = "medium"
-    STRONG = "strong"
-    VERY_STRONG = "very_strong"
+    INCLUDE_UPPER = "include_upper"
+    INCLUDE_DIGITS = "include_digits"
+    INCLUDE_SYMBOLS = "include_symbols"
 
 
 config_options = {
@@ -37,19 +38,11 @@ config_options = {
 }
 
 
-pass_data = {
-    "letters_lower": string.ascii_lowercase,
-    "digits": string.digits,
-    "letters_upper": string.ascii_uppercase,
-    "symbols": "!@#$%^&*()|/",
-}
-
-# sequence = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-:<=>?@[]_/"
 char_sequence = {
-    "lower": string.ascii_lowercase,
-    "upper": string.ascii_uppercase,
-    "digits": string.digits,
-    "symbols": "!#$%&'()*+-:<=>?@[]_/",
+    ConstPassConfig.STRING_LOWER: string.ascii_lowercase,
+    ConstPassConfig.STRING_UPPER: string.ascii_uppercase,
+    ConstPassConfig.STRING_NUMBERS: string.digits,
+    ConstPassConfig.STRING_SYMBOLS: "!#$%&'()*+-:<=>?@[]_/",
 }
 
 
@@ -62,18 +55,13 @@ def get_pass_length() -> int:
                 return length
             raise ValueError()
         except ValueError:
-            print("Password must be at least 4 characters long")
-
-
-patterns = {
-    ConstPatternStrong.WEAK: r"^[a-z]+$",
-    ConstPatternStrong.MEDIUM: r"^[A-Za-z]+$",
-    ConstPatternStrong.STRONG: r"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[A-Za-z\d]+$",
-    ConstPatternStrong.VERY_STRONG: r"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!#$%&'()*+-:<=>?@[]_/]).+$",
-}
+            cprint(
+                "\nPassword must be at least 4 characters long\n", ConstColors.COLOR_RED
+            )
 
 
 def get_pass_config() -> dict:
+    """Prompts the user to choose password configurations"""
     pass_config = []
     for option in config_options[ConstPassConfig.CONFIG_OPTIONS]:
         user_input = input(option).lower().strip()
@@ -83,63 +71,41 @@ def get_pass_config() -> dict:
         else:
             pass_config.append(False)
 
-    return {"1": pass_config[0], "2": pass_config[1], "3": pass_config[2]}
+    return {
+        ConstPassConfig.INCLUDE_UPPER: pass_config[0],
+        ConstPassConfig.INCLUDE_DIGITS: pass_config[1],
+        ConstPassConfig.INCLUDE_SYMBOLS: pass_config[2],
+    }
 
 
-pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?:{}|<>]).{4,}$"
-
-
-def generate_password(sequence, length):
+def generate_password(sequence: str, length: int) -> str:
+    """Generates a password based on user's configurations"""
     password = "".join(random.choices(sequence, k=length))
     return password
 
 
-def validate_password():
-    pass
-
-
-def get_sequence(char_sequence, settings):
+def get_sequence(char_sequence: str, settings: dict) -> str:
     """Returns one of the 8 possible variants of the character sequence string"""
-    sequence = char_sequence["lower"]
-    # true false false
-    if settings["1"] and not settings["2"] and not settings["3"]:
-        return sequence + char_sequence["upper"]
-    # false true false
-    if not settings["1"] and settings["2"] and not settings["3"]:
-        return sequence + char_sequence["digits"]
-    # false false true
-    if not settings["1"] and not settings["2"] and settings["3"]:
-        return sequence + char_sequence["symbols"]
-    # true true false
-    if settings["1"] and settings["2"] and not settings["3"]:
-        return sequence + char_sequence["upper"] + char_sequence["digits"]
-    # false true true
-    if not settings["1"] and settings["2"] and settings["3"]:
-        return sequence + char_sequence["digits"] + char_sequence["symbols"]
-    # true false true
-    if settings["1"] and not settings["2"] and settings["3"]:
-        return sequence + char_sequence["upper"] + char_sequence["symbols"]
-    # true true true
-    if settings["1"] and settings["2"] and settings["3"]:
-        return (
-            char_sequence["lower"]
-            + char_sequence["upper"]
-            + char_sequence["digits"]
-            + char_sequence["symbols"]
-        )
+    sequence = char_sequence[ConstPassConfig.STRING_LOWER]
+
+    if settings[ConstPassConfig.INCLUDE_UPPER]:
+        sequence += char_sequence[ConstPassConfig.STRING_UPPER]
+
+    if settings[ConstPassConfig.INCLUDE_DIGITS]:
+        sequence += char_sequence[ConstPassConfig.STRING_NUMBERS]
+
+    if settings[ConstPassConfig.INCLUDE_SYMBOLS]:
+        sequence += char_sequence[ConstPassConfig.STRING_SYMBOLS]
 
     return sequence
 
 
 def main():
+    """Runs pass generator programm"""
 
     length = get_pass_length()
     settings = get_pass_config()
-    print(settings)
-    # pattern = get_pattern(settings, patterns)
-    # print(pattern)
     sequence = get_sequence(char_sequence, settings)
-    print(sequence)
     password = generate_password(sequence, length)
     print(password)
 
